a4824c0f88f56ce93e1d68f648f19a867a3f50ae
Andresssg <andres-guevara1@upc.edu.co>
2023-05-09 01:11:19 -0500

    Add user model

--- talentplace/models.py
+++ talentplace/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    id = models.AutoField(primary_key = True)
+    name = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    birthdate = models.DateField()
+    phone = models.CharField(max_length=20)
+    gender = models.CharField(max_length=11)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    rol = models.CharField(max_length=11)
