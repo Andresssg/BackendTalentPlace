7420fe37b3d0fc5437f8dd97801351e5e42f565b
jupbc <jupbcan100@hotmail.com>
2023-05-29 21:19:41 -0500

    Login changes to verify encripted passwords

--- talentplace/backends.py
+++ talentplace/backends.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.backends import BaseBackend
+from django.contrib.auth.hashers import check_password
 from .models import User
 
 class UserBackend(BaseBackend):
     def authenticate(self, request, email=None, password=None):
         try:
             user = User.objects.get(email=email)
-            if user.check_password(password):
+            if check_password(password, user.password):
                 return user
         except User.DoesNotExist:
             return None
