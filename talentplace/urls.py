2399f04dff87aa15990157b2d320035be6729746
jupbc <jupbcan100@hotmail.com>
2023-05-29 19:52:08 -0500

    Add register and password encriptation

--- talentplace/urls.py
+++ talentplace/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework.documentation import include_docs_urls
 from rest_framework import routers
 from talentplace import views
+from .views import user_register
 
 router = routers.DefaultRouter()
 router.register(r"roles", views.RolView, 'roles')
@@ -11,8 +12,11 @@ router.register(r"services", views.ServiceView, 'services')
 router.register(r"offeredservices", views.OfferedServiceView, 'offeredservices')
 router.register(r"hiredservices", views.HiredServiceView, 'hiredservices')
 
+BASE_DIR = "api/v1/"
+
 urlpatterns = [
     #path("ruta",funcion a ejecutar, )
-    path("api/v1/", include(router.urls)),
+    path(f"{BASE_DIR}", include(router.urls)),
+    path(f"{BASE_DIR}register", user_register, name='register'),
     path("docs/", include_docs_urls(title="Talentplace API"))
 ]
