644c42f6c783d67757d529f63c6f4a82f7c65197
Andresssg <andres-guevara1@upc.edu.co>
2023-05-31 14:39:10 -0500

    Fix import views and add hireservices endepoint

--- talentplace/urls.py
+++ talentplace/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path, include
 from rest_framework.documentation import include_docs_urls
 from rest_framework import routers
-from talentplace import views
-from .views import user_register, user_login
+from .views import *
+
 
 router = routers.DefaultRouter()
-router.register(r"roles", views.RolView, 'roles')
-router.register(r"users", views.UserView, 'users')
-router.register(r"categories", views.CategoryView, 'categories')
-router.register(r"services", views.ServiceView, 'services')
-router.register(r"offeredservices", views.OfferedServiceView, 'offeredservices')
-router.register(r"hiredservices", views.HiredServiceView, 'hiredservices')
+router.register(r"roles", RolView, 'roles')
+router.register(r"users", UserView, 'users')
+router.register(r"categories", CategoryView, 'categories')
+router.register(r"services", ServiceView, 'services')
+router.register(r"offeredservices", OfferedServiceView, 'offeredservices')
+router.register(r"hiredservices", HiredServiceView, 'hiredservices')
 
 BASE_DIR = "api/v1/"
 
@@ -19,5 +19,6 @@ urlpatterns = [
     path(f"{BASE_DIR}", include(router.urls)),
     path(f"{BASE_DIR}register", user_register, name='register'),
     path(f"{BASE_DIR}login", user_login, name='login'),
+    path(f"{BASE_DIR}hireservice", hire_service, name='hireservice'),
     path("docs/", include_docs_urls(title="Talentplace API"))
 ]
